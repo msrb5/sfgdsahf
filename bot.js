fe7531fd2139fa2ce9ab8871908da2bd66442525
msrb5 <46552492+msrb5@users.noreply.github.com>
2019-01-10 10:09:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`R!play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`R!play | By ToBias.`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
